0b2694f755df3c9fa4f2c523fb5485cfde41474c
hootiehoooo <131828458+hootiehoooo@users.noreply.github.com>
2025-01-05 16:33:54 -0500

    Chaos

---
+++ FixingDept.ps1
@@ -0,0 +1,20 @@
+FixingDept.Ps1
+$All=Get-MgBetaUser -All | Select Department, Id
+$CostCenters=import-Csv -Path $env:USERPROFILE\Downloads\Costcenters.csv
+
+$Concat= $CostCenters | % {   
+$($_.CostCenter) +'-' + $($_.Department)
+    
+}
+
+
+
+ForEach ( $user in $All) {
+
+$UserDept=$Concat| Where { $_ -like "*-$($user.Department)"} 
+if ($UserDept) {
+    
+    Write-host "$($user.Id) $UserDept"
+    Update-MgBetaUser -UserId "$($user.Id)" -Department $UserDept
+}
+}
