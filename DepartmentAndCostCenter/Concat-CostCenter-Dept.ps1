01b5e82012ee4a35dd31082430939fa098f22d09
hootiehoooo <131828458+hootiehoooo@users.noreply.github.com>
2025-01-23 21:24:24 -0500

    added try catch for prettier output

--- DepartmentAndCostCenter/Concat-CostCenter-Dept.ps1
+++ DepartmentAndCostCenter/Concat-CostCenter-Dept.ps1
@@ -6,9 +6,6 @@ $Concat= $CostCenters | % {
     $($_.Department)+'-' + $($_.CostCenter) 
     
 }
-
-
-
 ForEach ($user in $All) {
 
 $UserDept= $Concat| Where { $_ -like "*$($user.Department)*"} 
@@ -16,13 +13,13 @@ $UserDept
 #$UserDept.GetType()
 
 if ($UserDept) {
-    $_
+   try { $_
     Write-host "$($user.Id) $UserDept"
     Update-MgBetaUser -UserId "$($user.Id)" -Department $UserDept
     
     
 }
- else {
+ catch {
     write-warning "no match found for $($user.Mail)'s dept"
     
     $data=[PSCustomObject]@{
@@ -32,6 +29,7 @@ if ($UserDept) {
  $WrongData+= $data
 }
 
+}
 }
 #Get Users
 #read csv
