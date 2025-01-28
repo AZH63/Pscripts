71c0b4538bdd6796dd95048878199ad4d866ecf9
Azaria Horton <ahorton@baldorfood.com>
2025-01-28 09:31:34 -0500

    fix csv filling

--- UpdateCustomAttribute1.ps1
+++ UpdateCustomAttribute1.ps1
@@ -1,32 +1,49 @@
 ﻿
-Connect-AzureAD
-Connect-ExchangeOnline
+$LicensedEnabled=[System.Collections.ArrayList]::new()
+$LicensedDisabled=[System.Collections.ArrayList]::new()
+$NonLicensed=[System.Collections.ArrayList]::new()
 $AllUsers=Get-AzureADUser -Filter "employeeId ge '0'" -All $true | Select UserPrincipalName,AccountEnabled, AssignedLicenses
 
-$Results= $AllUsers | ForEach-Object {
+$AllUsers | ForEach-Object {
 if ( $_.AccountEnabled -eq $false -and $_.AssignedLicenses -ne "null") {
 
 write-output " $($_.UserPrincipalName)  licensed and disabled $($_.AccountEnabled) "
 Set-Mailbox -Identity $_.UserPrincipalName -CustomAttribute1 "Disabled"
+[void]$LicensedDisabled.Add([PSCustomObject]@{
+    Name = $_.UserPrincipalName
+    License= $_.AssignedLicenses
+    AccountEnabled= $_.AccountEnabled
+    EmployeeType= $_.EmployeeType
+})
 
 }
 
 elseif ( $_.AccountEnabled -eq $true -and $_.AssignedLicenses -ne "null") {
 Write-Output " $($_.UserPrincipalName)  licensed and enabled $($_.AccountEnabled) "
 Set-Mailbox -Identity $_.UserPrincipalName -CustomAttribute1 $null
+[void]$LicensedEnabled.Add([PSCustomObject]@{
+    Name = $_.UserPrincipalName
+    License= $_.AssignedLicenses
+    AccountEnabled= $_.AccountEnabled
+    EmployeeType= $_.EmployeeType
+})
 
 }
 else  {
 Write-Output " $($_.UserPrincipalName) not licensed"
-
-}
-[PSCustomObject]@{
+[void]$NonLicensed.Add([PSCustomObject]@{
     Name = $_.UserPrincipalName
     License= $_.AssignedLicenses
     AccountEnabled= $_.AccountEnabled
-}
+    EmployeeType= $_.EmployeeType
+})
 
 }
-return $Results | Export-Csv -Path $env:UserProfile\Downloads -NoTypeInformation
 
+
+}
+return  
+$LicensedEnabled | Export-Csv -Path $env:UserProfile\Downloads\LicensedEnabled.csv -NoTypeInformation
+$LicensedDisabled| Export-Csv -Path $env:UserProfile\Downloads\LicensedDisabled.csv -NoTypeInformation
+$NonLicensed | Export-Csv -Path $env:UserProfile\Downloads\NonLicensed.csv -NoTypeInformation
 
