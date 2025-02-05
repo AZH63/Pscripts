a05370648a17c4c0d8e7b0576f9eed8e81ad872a
Azaria Horton <ahorton@baldorfood.com>
2025-02-05 17:54:51 -0500

    stuff

--- 365Tools/Test365Stuff.psm1
+++ 365Tools/Test365Stuff.psm1
@@ -39,6 +39,13 @@ end {
 
 
 
+Set-DynamicDistributionGroup -Identity BB_Sales -IncludedRecipients MailboxUsers `
+    -ConditionalDepartment "912", "Sales-912", "Sales" `
+    -ConditionalCompany "Boston", "MA" `
+    -ConditionalCustomAttribute1 "Disabled"
+
+
+    Set-DynamicDistributionGroup -Identity BB_Sales -RecipientFilter "(RecipientTypeDetails -eq 'UserMailbox') -and (Office -eq 'Boston' -or Office -eq 'MA') -and (Department -eq '912' -or Department -eq 'Sales-912' -or Department -eq 'Sales') -and (CustomAttribute1 -ne 'Disabled')"
 
 
 
@@ -50,3 +57,32 @@ end {
 
 $Groups= Get-MGGroup '"DisplayName:GroupName"'  -ConsistencyLevel eventual
 $AllGroups | % {Add-mgGroupMember -userid "$((Get-MgUser -filter " mail eq 'AdeleV@1x4bs0.onmicrosoft.com'").Id)" -Id  }
+
+
+
+Function Remove-DistMemb {
+    param (
+       
+    [parameter(Mandatory)]
+    [string]$identity,
+       [string[]]$member 
+       )
+    
+    begin {$group= $identity | % {  Get-DistributionGroup -Identity $identity} }
+    process {
+    
+        if ( $group)
+    $members= if ($group) { 
+    $group | % { Get-DistributionGroupMember -identity $_ | Select -ExpandProperty PrimarySmtpAddress }
+    }
+    else {
+        write-warning "group not found" 
+    }
+    
+    
+    
+    $members | % { remove-distributionGroupMember -identity $identity -member $_ -BypassSecurityGroupManagerCheck -Confirm:$false}
+    
+    }
+    end { }
+    }
