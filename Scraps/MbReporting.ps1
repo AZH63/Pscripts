dd901150384b88058c926638f909f999dadcbf06
Azaria Horton <ahorton@baldorfood.com>
2025-03-04 17:46:24 -0500

    Todo: Make a function and add to Reporting

---
+++ Scraps/MbReporting.ps1
@@ -0,0 +1,11 @@
+$Mbs=Get-Mailbox -InactiveMailboxOnly -ResultSize Unlimited | Select  DisplayName,PrimarySMTPAddress,WhenSoftDeleted, WhenChanged, SKUAssigned, RetentionPolicy,LitigationHoldEnabled,IsMailboxEnabled
+$Mbs | export-csv -path $env:UserProfile\Downloads\InactiveMbs.Csv -notypeinformation
+$Users= $Mbs.PrimarySMTPAddress
+
+$Users | % { Get-AzureADUser -ObjectId $_ | Select UserPrincipalName, AccountEnabled}
+ $Users | % {
+
+
+    Get-Mailbox -identity $_ | Select DisplayName, LitigationHoldEnabled
+ }
+ #Pls function-itize , add to reporting365 and reconsider this folder structure
