126171c8e48e6b093fe0ba6da9677440d7a5170c
hootiehoooo <131828458+AZH63@users.noreply.github.com>
2025-03-05 20:09:03 -0500

    fixed export csv exporting wrong thing changed get-date

--- Scraps/UnusedMbs.ps1
+++ Scraps/UnusedMbs.ps1
@@ -1,9 +1,8 @@
-Get-MailboxStatistics   -identity "yoohooo@1x4bs0.onmicrosoft.com" | Select-Object *
-#LastUserActionTime #last action performed by user
+
+#LastUserActionTime #last action performed by user -- deprecated
 #LastInteractionTime # near real time, updated independently but possible influenced by background assistants
 #LastLogonTime #ran MFA didn't seem to change, maybe it can be trusted
-Get-MgUserLicenseDetail 
-Get-MgBetaUser -ConsistencyLevel eventual | Select-Object * 
+
 
 
 $licensed= Get-AzureADUser | Where-Object { $_.AccountEnabled -eq $true -and $_.AssignedLicenses -ne $null}
@@ -22,11 +21,17 @@ $emails= $licensed | ForEach-Object {
      
 }
 $emails | export-csv -path $env:UserProfile\Downloads\MbLogonstats.csv
-[datetime]$date= Get-Date -format "MM/dd/yyyy HH:MM" 
+[datetime]$date= Get-Date 
 
-$nologonsinaweek = $emails | Where-Object { $_.LastLogonTime -and $_.LastLogonTime -lt $date.AddDays(-7) }
+
+$nologonsinaweek = $emails | Where-Object { $_.LastLogonTime -and $_.LastLogonTime -lt $date.AddDays(-7) } 
 $noLogonsInaMonth = $emails | Where-Object { !$_.LastLogonTime -or $_.LastLogonTime -lt $date.AddDays(-30) }
 $neverloggedIn= $emails | Where-Object { !$_.LastLogonTime}
 
 $nointeractions= $emails | Where-Object { !$_.LastInteractionTime}
+
+$nologonsinaweek | export-csv -path $env:USERPROFILE\Downloads\nologonsinaweek.csv
+$noLogonsInaMonth | export-csv -path $env:USERPROFILE\Downloads\noLogonsInaMonth.csv
+$neverloggedIn | export-csv -path $env:USERPROFILE\Downloads\neverloggedIn.csv
+$nointeractions | export-csv -path $env:USERPROFILE\Downloads\nointeractions.csv
 
