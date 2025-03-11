e55469398dc4b8055ff9d3bab924d98dc9d39736
hootiehoooo <131828458+AZH63@users.noreply.github.com>
2025-03-10 20:27:18 -0400

    Get-unusedmbs the function*tm added to reporting365

--- 365Tools/Reporting365.psm1
+++ 365Tools/Reporting365.psm1
@@ -69,25 +69,135 @@ Function Get-GroupInfoExport {
       }
   }
   
-Function Get-GroupInfo {
-  param (
-          [Parameter(Mandatory, ValueFromPipeline=$true)]
-          [string[]]$Searchstr
-      )
-  begin {
 
+Function get-Unusedmbs {
+    param(
+     [CmdletBinding()]
+    [Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+     [object[]]$users,
+     [switch]$week,
+     [switch]$nouserinteraction, #either find an alt or add description that its technically deprecated..making this excersise kinda useless
+     [switch]$month,
+     [switch]$nologon
+     
+  
+  )
+   begin {
+   
   }
-process {
-
-}
-end {
-
-}
-
-
-}
- 
-
+   process {
+     write-host "checking user- $_ entered"
+     $emails=$users | % {
+        if ($_ -notlike "*@*") {
+          write-verbose "not a UPN, checking "
+           try {
+              write-verbose "searching by displayname"
+             $res= Get-MgUser -Search "DisplayName:$_" -ConsistencyLevel eventual -ea Stop
+           # $res= Get-AzureADuser -searchstring "$_"
+            write-output "$($res.UserPrincipalName)"
+            
+        }
+        catch {
+           write-verbose "good ole where-object"
+          $res= Get-MgUSer | Where { $_.Name -like "*$_*" }
+          
+          write-output "$($res.UserPrincipalName)"
+         
+        
+           }
+       
+       }
+       else {
+          write-verbose "$_ all good here" 
+          write-output "$_"
+           }
+       
+        }
+        
+  $mbstats= foreach ($email in $emails) {
+     try {
+        Write-Host "grabbing mailbox statistics"
+   $stats= Get-MailboxStatistics -identity $email | Select LastInteractionTime, LastUserActionTime, LastLogonTime -ErrorAction stop -ErrorVariable err
+   
+              [PSCustomObject]@{
+                  Name       = $email
+                  LastLogonTime       = $stats.LastLogonTime
+                  LastInteractionTime = $stats.LastInteractionTime
+                  LastUserActionTime= $stats.LastUserActionTime
+              }
+          
+          [datetime]$date= Get-Date
+        }
+        catch {
+            $err
+  
+        }
+     }
+  }
+  
+  end 
+  {
+  
+  switch ($PSBoundParameters.keys) {
+     
+     'week' {
+        
+        $nologonweek= $mbstats | Where-Object { $_.LastLogonTime -and $_.LastLogonTime -lt $date.AddDays(-7) }
+        if (!$nologonweek) {
+             write-host "value is null"
+             
+        }
+        else {
+        $nologonweek | export-csv -path $env:UserProfile\Downloads\nologonweek.csv
+        }
+         
+     }
+     'month' {
+      $nologonmonth = $mbstats | Where-Object { $_.LastLogonTime -and $_.LastLogonTime -lt $date.AddDays(-30)  }
+      if (!$nologonmonth) {
+         write-host "value is null"
+        
+      }
+      else {
+      $nologonmonth | export-csv -path $env:UserProfile\Downloads\nologonweek.csv
+      }
+     }
+     'nologon' { $nologon= $mbstats | Where-Object { !$_.LastLogonTime } 
+      if (!$nologon) {
+             write-host "value is null"
+             
+      }
+      
+      else {
+     $nologon | export-csv -path $env:UserProfile\Downloads\nologon.csv
+      }
+  
+  }
+  
+     'nouserinteraction' {
+        $nouserinteraction= $mbstats | Where-Object {!$_.LastUserActionTime}
+        if (!$nouserinteraction) {
+            write-host "value is null"
+           
+        }
+        else {
+        $nouserinteraction | export-csv -path $env:UserProfile\Downloads\nouserinteraction.csv
+        }
+     }
+     default {    
+       $mbstats | export-csv -path $env:UserProfile\Downloads\mbstats.csv  
+    
+     }
+  
+    
+     }
+     
+  }
+  }
+  
+  
+  
+  
 
 
 
