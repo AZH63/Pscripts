e44427d7131945996165cfc334b26380a8f19d47
hootiehoooo <131828458+AZH63@users.noreply.github.com>
2025-04-02 00:50:42 -0400

    audit time. will eventually make reasonable looking and functions

---
+++ Scraps/LicensingAuditWithLogs.ps1
@@ -0,0 +1,42 @@
+
+
+#find name of parameter used to identify users which will be used 
+$Eidentifier= $emailreport | get-member | Where {$_.Name -like "*User Principal Name*" -or $_.Name -like "*UserPrincipalName*" -or $_.Name -like "*UPN*" }
+$Tidentifier= $teamsreport
+
+#or use graph 
+$teamsreport= import-csv -path $env:USERPROFILE\downloads\TeamsReport.csv
+$emailreport= import-csv -path $env:USERPROFILE\downloads\EmailReport.csv
+
+
+#arraylist to store obj and let us use Add method
+$unifiedAudit= [System.Collections.ArrayList]::new()
+
+foreach ($em in $emailreport){
+ 
+    $match= $teamsreport | Where {  $em.'User Principal Name' -match $_.'User Principal Name' } | Select * 
+
+
+    # $match, "data pulled from teams"   # sanity check
+  # $em ,"corresponding email report user" #sanity check
+  $unifiedAudit.Add([PSCustomObject]@{
+        UPN = $em.'User Principal Name'
+        "Exo last activity date"=$em."Last Activity Date" 
+        "Teams Last activity date"= $match."Last Activity Date"
+        "Team Chat Message Count"= if ($match) {$match.'Team Chat Message Count'} else { "no stats for this one in teams audit"} 
+        "Private Chat Message Count"= if ($match) { $match."Private Chat Message Count"} else { "no stats for this one in Exo audit" }
+        "Call Count"= $match."Call Count"
+        "Meetings Attended"= $match."Meetings Attended Count"
+        "Assigned Plans"= $match."Assigned Products"
+        "Send Count"= $em."Send Count"
+        "Receive Count"= $em."Receive Count"
+        "Exch Meeting Count"= $em."Meeting Count"
+        "Read mails"= $em."Read Count"
+
+     }) | Out-Null
+
+}
+
+
+$unifiedAudit | export-csv -path $env:UserProfile\downloads\UnifiedAudit.csv -NoTypeInformation
+start  $env:UserProfile\downloads\UnifiedAudit.csv
