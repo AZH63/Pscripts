f53a870cd07481aaf508bef57d7a70d8a441f124
hootiehoooo <131828458+AZH63@users.noreply.github.com>
2025-05-06 00:01:19 -0400

    licensing

---
+++ Scraps/ZDTest.ps1
@@ -0,0 +1,43 @@
+function Convert-weborders 
+{
+param (
+    [string]$domain
+)
+Get-Mailbox -filter "ForwardingSmtpAddress -like '$domain*'" |Select -expandProperty PrimarySmtpAddress tee-object -variable mbs
+$mbs | % {  
+write-verbose "converting to shared mailboxes"
+Set-mailbox -identity $_ -Type Shared
+#add it to csv and update csv with each iteration to track changes 
+}
+}
+
+function Convert-sku {
+    [CmdletBinding()]
+param (
+    [string]$upn,
+    [parameter(Mandatory=$true)]
+    [string]$skuname
+
+
+)
+  
+$license= ($PSBoundParameters.ContainsKey('upn'))?(Get-MgBetaUserLicenseDetail -userId $upn | select * ): (Get-MgBetaSubscribedSku)
+write-verbose "$license"
+
+$licenseinfo= $license | Where { $_.SkuPartNumber -like "*$($PSBoundParameters["skuname"])*"} | select SkuPartNumber,SkuId 
+if ($licenseinfo.Count -gt 1) {
+write-warning "be more specific, here are the skus found $license"
+return
+
+}
+else {
+New-Guid -InputObject "$($licenseinfo.SkuId)"
+}
+
+}
+
+
+
+
+
+
