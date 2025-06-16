a57955d3933d2fb1892c7d1228aa461458a95603
AZH63 <AZH63>
2025-06-16 17:02:18 -0400

    changed UpdateCustomAttribute1Graph

--- UpdateCustomAttribute1Graph.ps1
+++ UpdateCustomAttribute1Graph.ps1
@@ -1,15 +1,22 @@
 Get-MgBetaUser -All | Tee-Object -Variable users
 
 $users | % {
-  if (($_.AssignedLicenses -ne $null) -and ($_.EmployeeType -like "*terminated*")) {
+  if (($_.Mail -ne $null) -and ($_.EmployeeType -like "*terminated*")) {
     set-mailbox -identity $($_.UserPrincipalName) -CustomAttribute1 "disabled"
-" user flagged as $($_.UserPrincipalName) $($_.EmployeeType)"
-  Get-MgBetaUserLicensedetail -userid $($_.UserPrincipalName) | select SkuPartNumber
+" user flagged as termed $($_.UserPrincipalName) $($_.EmployeeType)"
+  
   }
-elseif (($_.AssignedLicenses -ne $null) -and ($_.EmployeeType -notlike "*terminated*") -and ($_.EmployeeId -ne $null)) {
+elseif (($_.Mail -ne $null) -and ($_.EmployeeType -notlike "*terminated*") -and ($_.EmployeeId -ne $null)) {
 
   "user $($_.UserPrincipalName) is $($_.EmployeeType)"
  set-mailbox -identity $_.UserPrincipalName -CustomAttribute1 $null
     
 }
+}
+
+
+forEach ($term in $termed) {
+" user $term"
+ Get-Mailbox -Identity $term | select PrimarySmtpAddress, CustomAttribute1
+
 }
