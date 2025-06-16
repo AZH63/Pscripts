4d5ed8e49c51f1be32d82717f0a2c39a12630595
AZH63 <AZH63>
2025-06-16 17:24:16 -0400

    often fails half way so trying to break it up

--- UpdateCustomAttribute1Graph.ps1
+++ UpdateCustomAttribute1Graph.ps1
@@ -1,12 +1,12 @@
 Get-MgBetaUser -All | Tee-Object -Variable users
 
 $users | % {
-  if (($_.Mail -ne $null) -and ($_.EmployeeType -like "*terminated*")) {
+  if (($_.AssignedLicenses -ne $null) -and ($_.EmployeeType -like "*terminated*")) {
     set-mailbox -identity $($_.UserPrincipalName) -CustomAttribute1 "disabled"
-" user flagged as termed $($_.UserPrincipalName) $($_.EmployeeType)"
-  
+" user flagged as $($_.UserPrincipalName) $($_.EmployeeType)"
+  Get-MgBetaUserLicensedetail -userid $($_.UserPrincipalName) | select SkuPartNumber
   }
-elseif (($_.Mail -ne $null) -and ($_.EmployeeType -notlike "*terminated*") -and ($_.EmployeeId -ne $null)) {
+elseif (($_.AssignedLicenses -ne $null) -and ($_.EmployeeType -notlike "*terminated*") -and ($_.EmployeeId -ne $null)) {
 
   "user $($_.UserPrincipalName) is $($_.EmployeeType)"
  set-mailbox -identity $_.UserPrincipalName -CustomAttribute1 $null
@@ -14,9 +14,32 @@ elseif (($_.Mail -ne $null) -and ($_.EmployeeType -notlike "*terminated*") -and 
 }
 }
 
+Get-MgBetaUser -All | Tee-Object -Variable users
+$termed= $users | Where { $_.EmployeeType -like "*Terminated*"}
+$termed | % {
+   try { 
+  Set-mailbox -Identity $($_.Mail) -CustomAttribute1 "disabled" -ErrorAction Stop
+   }
+   catch {
+  write-warning "no mailbox for user $($_.Mail)"
+   }
+}
+$active= $users | Where { $_.EmployeeType -like "*Active*"}
+$active | % {
+  try {
+  Set-Mailbox -identity $($_.Mail) -CustomAttribute1 "$null" -ErrorAction Stop
+  }
+  catch {
+write-warning "no mailbox for user $($_.Mail)"
+  }
+
+}
+
+
+
 
 forEach ($term in $termed) {
 " user $term"
- Get-Mailbox -Identity $term | select PrimarySmtpAddress, CustomAttribute1
+Get-Mailbox -Identity $term | select PrimarySmtpAddress, CustomAttribute1
 
 }
