f96c35042859fd8e9c34ed53d80efecad98d62bd
AZH63 <AZH63>
2025-06-17 15:09:43 -0400

    cleanup

--- UpdateCustomAttribute1Graph.ps1
+++ UpdateCustomAttribute1Graph.ps1
@@ -1,18 +1,4 @@
-Get-MgBetaUser -All | Tee-Object -Variable users
 
-$users | % {
-  if (($_.AssignedLicenses -ne $null) -and ($_.EmployeeType -like "*terminated*")) {
-    set-mailbox -identity $($_.UserPrincipalName) -CustomAttribute1 "disabled"
-" user flagged as $($_.UserPrincipalName) $($_.EmployeeType)"
-  Get-MgBetaUserLicensedetail -userid $($_.UserPrincipalName) | select SkuPartNumber
-  }
-elseif (($_.AssignedLicenses -ne $null) -and ($_.EmployeeType -notlike "*terminated*") -and ($_.EmployeeId -ne $null)) {
-
-  "user $($_.UserPrincipalName) is $($_.EmployeeType)"
- set-mailbox -identity $_.UserPrincipalName -CustomAttribute1 $null
-    
-}
-}
 
 Get-MgBetaUser -All | Tee-Object -Variable users
 $termed= $users | Where { $_.EmployeeType -like "*Terminated*"}
