816a86ec6bffcc0d068f367fc30f333b07a4abce
AZH63 <hortazaria@gmail.com>
2025-09-26 13:43:49 -0700

    working..

--- Scraps/Intunetest.ps1
+++ Scraps/Intunetest.ps1
@@ -1,15 +1,16 @@
 
 
 function rename-devices {
-
+  [CmdletBinding()]
 param(
 [Parameter(Mandatory=$true)]
 [string[]]$newname,
 [parameter(Mandatory=$true)]
-[string[]]$deviceids
+[string[]]$deviceids,
+[string]$path="$env:userprofile\downloads\"
 )
-
-
+$succeslog=[System.Collections.ArrayList]::new()
+$errorlog=[System.Collections.ArrayList]::new()
 #if ($PSBoundParameters["$($([array]$PSBoundParameters.keys)[0])"].Count -ne $PSBoundParameters["$($([array]$PSBoundParameters.keys)[1])"].Count ){
 if ($newname.Count -ne $deviceids.Count) {
 write-warning "equal number of new names and device ids please"
@@ -26,18 +27,45 @@ $hash=$PSBoundParameters | % {
     }
    
    }
-}
 
 
 forEach ($device in $hash) {
     
     write-verbose "looking for device to rename to $($device.newname)"
-    Get-MgDeviceManagementManagedDevice -filter "deviceName eq '$($device.newname)'"
+    $dupe= Get-MgDeviceManagementManagedDevice -filter "deviceName eq '$($device.newname)'"
+    if ($null -eq $dupe) {
+        try {
+          write-verbose "attempting rename"
+            Set-MgBetaDeviceManagementManagedDeviceName -managedDeviceId $($device.deviceid) -DeviceName $($device.newname) -ErrorAction Stop
+        Sync-MgBetaDeviceManagementManagedDevice -ManagedDeviceId $($device.deviceid)
+        
+        }
+        catch {
+              
+            $errorlog.Add( "$($device.NewName) failed to rename $error[0]")
 
+        }
+        }
+    else {
+
+        "dupe detected, skipping"
+          
+        $errorlog.Add("$($device.newname)- is a dupe $($device.deviceid)")
+        return
+        
+    }
+    
 
 
 
 }
+$errorlogs | out-file -path "$path" +"errorlogs.txt"
+$sucesslog | out-file -path "$path"+ "successlog.txt"
+
+}
+
+
+
 
 
 #doesn't exist in intune already
@@ -67,3 +95,8 @@ function Get-BoundParameters {
 $PSBoundParameters
 
 }
+
+
+$users=Get-MgBetauser -all 
+
+$operations= $users | Where { $_.Department -like "*operations*" -or $_.Department -like "*Warehouse*" -or $_.Department -like "*420*" -and $_.EmployeeType -like "*Administrative*" }
